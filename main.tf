7b84be0074dd409febee939f71ef92731e690a5f
hinling yeung <hinling.yeung@ihealthlabs.com>
2018-03-27 14:43:38 -0700

    taking out the self.id and shell.id

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "null_resource" "shell" {
   }
 
   provisioner "local-exec" {
-    command = "${local.command_chomped} 2>\"${path.module}/stderr.${self.id}\" >\"${path.module}/stdout.${self.id}\"; echo $? >\"${path.module}/exitstatus.${self.id}\""
+    command = "${local.command_chomped} 2>\"${path.module}/stderr\" >\"${path.module}/stdout\"; echo $? >\"${path.module}/exitstatus\""
   }
 
   provisioner "local-exec" {
@@ -27,19 +27,19 @@ resource "null_resource" "shell" {
 
   provisioner "local-exec" {
     when       = "destroy"
-    command    = "rm \"${path.module}/stdout.${self.id}\""
+    command    = "rm \"${path.module}/stdout\""
     on_failure = "continue"
   }
 
   provisioner "local-exec" {
     when       = "destroy"
-    command    = "rm \"${path.module}/stderr.${self.id}\""
+    command    = "rm \"${path.module}/stderr\""
     on_failure = "continue"
   }
 
   provisioner "local-exec" {
     when       = "destroy"
-    command    = "rm \"${path.module}/exitstatus.${self.id}\""
+    command    = "rm \"${path.module}/exitstatus\""
     on_failure = "continue"
   }
 }
@@ -48,9 +48,9 @@ data "external" "read" {
   program = ["ruby", "${path.module}/read.rb"]
 
   query = {
-    stdout     = "${path.module}/stdout.${null_resource.shell.id}"
-    stderr     = "${path.module}/stderr.${null_resource.shell.id}"
-    exitstatus = "${path.module}/exitstatus.${null_resource.shell.id}"
+    stdout     = "${path.module}/stdout"
+    stderr     = "${path.module}/stderr"
+    exitstatus = "${path.module}/exitstatus"
   }
 }
 
